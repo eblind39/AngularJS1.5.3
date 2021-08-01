1c2308e9890ba13c108c1e50f57f21eb97b22596
Ernesto Gutierrez <ergutierrez39@gmail.com>
2021-08-01 15:54:56 -0600

    feat: http sample, get & post data to https://jsonplaceholder.typicode.com/

--- Assets/scripts/httpsample.js
+++ Assets/scripts/httpsample.js
@@ -2,6 +2,9 @@
 let MyNGApp = angular.module("MyNGApp", []);
 MyNGApp.controller("MyNGController", ["$scope", "$http", function(s, h) {
     s.posts = [];
+    s.users = [];
+    s.newPost = {};
+
     h.get("https://jsonplaceholder.typicode.com/posts")
         .success(function(data) {
             s.posts = data;
@@ -14,10 +17,10 @@ MyNGApp.controller("MyNGController", ["$scope", "$http", function(s, h) {
     s.bindUsers = function() {
         h.get("https://jsonplaceholder.typicode.com/users")
         .success(function(data) {
-            let users = data;
+            s.users = data;
             s.posts.map(post => {
-                let author = users.find(user => user.id == post.userId);
-                post.author = { name: '', email: '' }
+                let author = s.users.find(user => user.id == post.userId);
+                post.author = { name: '', email: '' };
                 post.author.name = author.name;
                 post.author.email = author.email;
             });
@@ -25,5 +28,29 @@ MyNGApp.controller("MyNGController", ["$scope", "$http", function(s, h) {
         .error(function(err) {
             console.log(err);
         });
+    }
+
+    s.addPost = function() {
+        let post = {
+            title: s.newPost.title,
+            body: s.newPost.body,
+            userId: s.userSelected,
+        };
+        let author = s.users.find(user => user.id == s.userSelected);
+        console.log(author, s.userSelected);
+        s.newPost.author = { name: '', email: '' };
+        s.newPost.author.name = author.name;
+        s.newPost.author.email = author.email;
+        
+        h.post("https://jsonplaceholder.typicode.com/posts", post)
+            .success(function(result, status, headers, config) {
+                console.log(result);
+                s.posts.push(s.newPost);
+                s.newPost = {};
+                s.userSelected = null;
+            })
+            .error(function(err, status, headers, config) {
+
+            });
     }
 }]);
