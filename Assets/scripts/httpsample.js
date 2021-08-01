8c3cd527955d17b720a790b7c97a01a0bbad2feb
Ernesto Gutierrez <ergutierrez39@gmail.com>
2021-08-01 15:16:22 -0600

    fix: author object in post was fixed to start as an empty object

--- Assets/scripts/httpsample.js
+++ Assets/scripts/httpsample.js
@@ -1,12 +1,29 @@
+'use strict';
 let MyNGApp = angular.module("MyNGApp", []);
 MyNGApp.controller("MyNGController", ["$scope", "$http", function(s, h) {
     s.posts = [];
     h.get("https://jsonplaceholder.typicode.com/posts")
         .success(function(data) {
-            console.log(data);
             s.posts = data;
+            s.bindUsers();
         })
         .error(function(err) {
             console.log(err);
         });
+    
+    s.bindUsers = function() {
+        h.get("https://jsonplaceholder.typicode.com/users")
+        .success(function(data) {
+            let users = data;
+            s.posts.map(post => {
+                let author = users.find(user => user.id == post.userId);
+                post.author = { name: '', email: '' }
+                post.author.name = author.name;
+                post.author.email = author.email;
+            });
+        })
+        .error(function(err) {
+            console.log(err);
+        });
+    }
 }]);
